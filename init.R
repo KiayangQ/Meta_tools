13e418ed063b2cfb3d8e76035d4b9ebee8dd3766
KiayangQ <k.qin@uu.nl>
2020-10-16 21:42:22 +0200

    INSTALL PACKAGE

---
+++ init.R
@@ -0,0 +1,14 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+
+my_packages = c("shinydashboard", "dashboardthemes", "shinyalert","shinybusy","bs4Dash","shinyjs","DT","revtools","magrittr","stringr","dplyr","tidyr")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
